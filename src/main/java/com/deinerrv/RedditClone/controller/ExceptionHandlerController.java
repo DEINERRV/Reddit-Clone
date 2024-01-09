1d79fd1428150e18d78ef50064479d46bc4367b9
DEINERRV <deiner.ra10@gmail.com>
2024-01-09 13:39:32 -0600

    fix: bug fixed

--- src/main/java/com/deinerrv/RedditClone/controller/ExceptionHandlerController.java
+++ src/main/java/com/deinerrv/RedditClone/controller/ExceptionHandlerController.java
@@ -1,9 +1,9 @@
 package com.deinerrv.RedditClone.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.deinerrv.RedditClone.exception.SpringRedditException;
 
@@ -14,9 +14,8 @@ import lombok.extern.slf4j.Slf4j;
 public class ExceptionHandlerController {
     
     @ExceptionHandler(SpringRedditException.class)
-    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
-    public String err(SpringRedditException err){
+    public ResponseEntity<String> err(SpringRedditException err){
         log.info(err.getMessage());
-        return err.getMessage();
+        return new ResponseEntity<>(err.getMessage(),HttpStatus.BAD_REQUEST);
     }
 }
